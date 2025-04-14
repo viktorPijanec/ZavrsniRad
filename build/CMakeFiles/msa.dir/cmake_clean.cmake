93476946bbd2263d1e0cb8b0cfa39d2361612df1
viktorPijanec <viktor.pijanec@fer.unizg.hr>
2025-04-14 17:55:37 +0200

    build cijeli

---
+++ build/CMakeFiles/msa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msa.dir/src/msa.cpp.o"
+  "CMakeFiles/msa.dir/src/msa.cpp.o.d"
+  "msa"
+  "msa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/msa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
